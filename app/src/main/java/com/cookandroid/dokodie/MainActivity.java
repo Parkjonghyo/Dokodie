58267bab5c22dcab918818c85ed61f30e4e0bb19
dahuiLim <s18056@gsm.hs.kr>
2019-12-10 18:25:10 +0900

    액티비티전환
    액티비티 쌓이는거 없앰
    전환 에니메이션

--- app/src/main/java/com/cookandroid/dokodie/MainActivity.java
+++ app/src/main/java/com/cookandroid/dokodie/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cookandroid.dokodie;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,10 +13,13 @@ public class MainActivity extends AppCompatActivity {
     Button newBtn, bestBtn, calBtn;
     LinearLayout book1;
 
+    public static Activity newB_Activity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //Intent intent = new Intent(this, ParsingActivity.class);
         //startActivity(intent);
@@ -25,11 +29,15 @@ public class MainActivity extends AppCompatActivity {
         calBtn=findViewById(R.id.calendar);
         book1=findViewById(R.id.book1);
 
+        newB_Activity=MainActivity.this;
+
         bestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, BestSellerActivity.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_left);
+                newB_Activity.finish();
             }
         });
 
@@ -38,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent=new Intent(MainActivity.this, BookActivity.class);
                 startActivity(intent);
+                overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_left);
             }
         });
     }
